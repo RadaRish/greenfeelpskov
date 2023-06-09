06155ebcba9e4ee74147da6443c9a8e34350509b
RadaRish <rada.rish@ya.ru>
2023-06-09 17:02:37 +0300

    обновлен тур с названиями

---
+++ data.js
@@ -0,0 +1,527 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0------",
+      "name": "заезд во двор с ул Металлистов",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.04379189759550073,
+        "pitch": 0.015309442603022916,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.045420920309524604,
+          "pitch": 0.00496845616846997,
+          "rotation": 0,
+          "target": "1----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1----",
+      "name": "Общественная парковка при отеле",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.048163596091932,
+        "pitch": 0.09570189735953605,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9473739631202953,
+          "pitch": 0.02289881067953914,
+          "rotation": 0,
+          "target": "0------"
+        },
+        {
+          "yaw": -1.0654366225456684,
+          "pitch": 0.02026199191848832,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Вход в отель",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.010094050898810991,
+        "pitch": -0.130175793204522,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14680763661904095,
+          "pitch": 0.16564983909227848,
+          "rotation": 0,
+          "target": "3---"
+        },
+        {
+          "yaw": -2.553652777902837,
+          "pitch": 0.07986894369472353,
+          "rotation": 0,
+          "target": "1----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---",
+      "name": "Стойка регистрации (ресепшен)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.907255639212778,
+        "pitch": 0.06125039434608226,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6309108742141056,
+          "pitch": 0.1133555326424478,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": -0.7804305278865797,
+          "pitch": 0.22487326754182568,
+          "rotation": 0,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "Холл отеля",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.17191774123074666,
+        "pitch": 0.032295169511407096,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08180271244152948,
+          "pitch": 0.09872354629063196,
+          "rotation": 0,
+          "target": "6---2"
+        },
+        {
+          "yaw": -2.281549120202202,
+          "pitch": 0.297978637825004,
+          "rotation": 0,
+          "target": "5-----1-"
+        },
+        {
+          "yaw": 1.5881294864820585,
+          "pitch": 0.38091246633455356,
+          "rotation": 0,
+          "target": "3---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-----1-",
+      "name": "Стандартный двухместный номер с 1 кроватью",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.4101167539300477,
+        "pitch": 0.12549473339682216,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4133336298081076,
+          "pitch": 0.2306021155911573,
+          "rotation": 0,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6---2",
+      "name": "Холл отеля 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.095507422810254,
+        "pitch": 0.11417053191381044,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0758918007062057,
+          "pitch": 0.41057192192557856,
+          "rotation": 0,
+          "target": "7-----1--2----"
+        },
+        {
+          "yaw": -0.19475659215764196,
+          "pitch": 0.09586786631547639,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": 2.9859928393136395,
+          "pitch": 0.16042277046582143,
+          "rotation": 0,
+          "target": "8---3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-----1--2----",
+      "name": "Улучшенный двухместный номер с 1 или 2 отдельными кроватями (с доп.местом)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.8065074871041737,
+        "pitch": 0.14546688398649188,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9802672391428864,
+          "pitch": 0.23700795410159614,
+          "rotation": 0,
+          "target": "6---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8---3",
+      "name": "Холл отеля 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.1391389977625,
+        "pitch": -0.013946758275448445,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.769058489076474,
+          "pitch": 0.2423313888465195,
+          "rotation": 0,
+          "target": "9--"
+        },
+        {
+          "yaw": 1.5523784409118875,
+          "pitch": 0.09932539569522802,
+          "rotation": 0,
+          "target": "11--1"
+        },
+        {
+          "yaw": -0.05194103318717502,
+          "pitch": 0.1618668380736903,
+          "rotation": 0,
+          "target": "6---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9--",
+      "name": "Общая зона",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.0921070190968987,
+        "pitch": 0.24195252949752621,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.867209676877298,
+          "pitch": 0.25022594727670366,
+          "rotation": 0,
+          "target": "8---3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10--2",
+      "name": "Апартаменты (студия)2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.480189345827501
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12957474709878447,
+          "pitch": 0.14971751811063427,
+          "rotation": 0,
+          "target": "11--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11--1",
+      "name": "Апартаменты (студия)1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03133568385096552,
+          "pitch": 0.20520410329638317,
+          "rotation": 0,
+          "target": "10--2"
+        },
+        {
+          "yaw": -3.0601636853161303,
+          "pitch": 0.35304918612083114,
+          "rotation": 0,
+          "target": "8---3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
